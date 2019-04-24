b1a1288175308c56becccd9f20dc9d602fabd03b
Ozgur Asar <ozgurasarstat@gmail.com>
2019-04-24 10:59:11 +0300

    update

--- R/run.R
+++ R/run.R
@@ -1,22 +1,21 @@
-# library(nlme)
-# library(rstan)
-# library(tidyverse)
-# library(lmenssp)
-#
-#
-# data("data.sim.ibm")
-# data <- data.sim.ibm
-#
-# ## fit the normal model
-# fit <- wrap(fixed = log.egfr ~ bage + fu,
-#             random = ~ fu,
-#             data = data,
-#             id = "id",
-#             model = "exp",
-#             chains = 1,
-#             timeVar = "fu",
-#             cores = 1,
-#             iter = 2000,
-#             warmup = 1000,
-#             control = list(adapt_delta = 0.8))
-# print(fit, pars = c("alpha", "Sigma", "sigma_W", "range", "sigma_Z"))
+library(stochasticjm)
+library(lmenssp)
+
+data("data.sim.ibm")
+
+data <- data.sim.ibm[unique(data.sim.ibm$id)[1:50], ]
+
+## fit the normal model
+fit <- wrap(fixed = log.egfr ~ bage + fu,
+            random = ~ fu,
+            data = data,
+            id = "id",
+            model = "exp",
+            chains = 1,
+            timeVar = "fu",
+            priors = list(sigma_W = 1),
+            cores = 1,
+            iter = 2000,
+            warmup = 1000,
+            control = list(adapt_delta = 0.99))
+print(fit, pars = c("alpha", "Sigma", "sigma_W", "range", "sigma_Z"), digits_summary = 2)
